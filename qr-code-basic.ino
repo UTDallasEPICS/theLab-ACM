1d5400167fbd880b3ee1c805d2a401ef45a7c45a
Aadit Aggarwal <113587949+AaditAggarwal@users.noreply.github.com>
2024-10-28 18:10:47 -0500

    Uploaded the updated codes
    Updated the code for using 2 LEDs as visual cues about the state of scan of QR-Code
    Also established UART 1 with IO19 pin on ESP32.

--- qr-code-basic.ino
+++ qr-code-basic.ino
@@ -5,53 +5,55 @@
 ESP32QRCodeReader reader(CAMERA_MODEL_AI_THINKER);
 
 //const int redPin = 33;
-const int flashPin = 4;
+const int goodPin = 15;
+const int badPin = 14;
 #define TX_PIN 12
 
 void sendToESP32(const char* data) {
-  Serial2.println(data); // Send the data via Serial to ESP-WROOM-32
+  Serial1.println(data); // Send the data via Serial to ESP-WROOM-32
 }
 
 void onQrCodeTask(void *pvParameters)
 {
   struct QRCodeData qrCodeData;
-  // digitalWrite(flashPin, HIGH);
-  // delay(1500);
-  // digitalWrite(flashPin, LOW);
+  digitalWrite(goodPin, LOW);
+  digitalWrite(badPin, LOW);
   while (true){
     if (reader.receiveQrCode(&qrCodeData, 100))
     {
       Serial.println("Found QRCode");
       if (qrCodeData.valid)
       {
+        digitalWrite(goodPin, HIGH);
         Serial.print("Payload: ");
         Serial.println((const char *)qrCodeData.payload);
         sendToESP32((const char *)qrCodeData.payload);
-        // digitalWrite(flashPin, HIGH);
-        // delay(500);
-        // digitalWrite(flashPin, LOW);
-        // delay(500);
+        delay(5000);
+        digitalWrite(goodPin, LOW);
+        break;
       }
       else
       {
         Serial.print("Invalid: ");
         Serial.println((const char *)qrCodeData.payload);
         sendToESP32("Invalid Data");
-        // for (int i = 0; i <2; i++){
-        //   digitalWrite(flashPin, HIGH);
-        //   delay(200);
-        //   digitalWrite(flashPin, LOW);
-        //   delay(200);
-        // }
+        for (int i = 0; i <2; i++){
+          digitalWrite(badPin, HIGH);
+          delay(250);
+          digitalWrite(badPin, LOW);
+          delay(250);
+        }
       }
     }
     vTaskDelay(100 / portTICK_PERIOD_MS);
   }
+  xTaskCreate(onQrCodeTask, "onQrCode", 4 * 1024, NULL, 4, NULL);
 }
 
 void setup()
 {
-  pinMode(flashPin, OUTPUT);
+  pinMode(goodPin, OUTPUT);
+  pinMode(badPin, OUTPUT);
   Serial.begin(115200);
   Serial1.begin(9600, SERIAL_8N1, -1, TX_PIN);  // Only TX on GPIO12 (to ESP-WROOM-32 RX)
 
@@ -69,5 +71,5 @@ void setup()
 
 void loop()
 {
-  delay(2000);
+  //delay(2000);
 }
